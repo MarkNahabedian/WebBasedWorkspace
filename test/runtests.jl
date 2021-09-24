8e9a71cd3053356bc07229754ad87059e7e01bcb
MarkNahabedian <naha@mit.edu>
2021-09-23 21:27:20 -0400

    Minimal test.  Pkg.add of the resulting PackageSpecs succeeded.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using WebBasedWorkspace
 using Test
+using URIs
+using Pkg
+
+EXAMPLE_URI = URI("https://github.com/MarkNahabedian/WebBasedWorkspace")
 
 @testset "WebBasedWorkspace.jl" begin
-    # Write your tests here.
+    pspecs = package_specs(github_raw(EXAMPLE_URI))
+    @test length(pspecs) == 4
+    Pkg.add(pspecs)
 end
+
